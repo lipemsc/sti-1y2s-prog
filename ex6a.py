1461b12ab87006273a2c07dabd158f67a50c2d62
lipemsc <lipe.msc@gmail.com>
2022-03-02 21:41:20 +0000

    Changed ex6a for the correct file

--- ex6a.py
+++ ex6a.py
@@ -1,7 +1,16 @@
-def pedirnome():
-  return input("Insira o seu nome: ")
+# 6) A - Faça uma função que recebe 2 strings e informe se possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
 
-def nomeaocontrario(nome):
-    print(nome[::-1])
- 
-nomeaocontrario(pedirnome())
+def verificar_string(str1, str2):
+    print(f"Tamanho de \"{str1}\": {len(str1)} caracteres")
+    print(f"Tamanho de \"{str2}\": {len(str2)} caracteres")
+
+    if len(str1) == len(str2):
+        print("As duas strings são de tamanhos iguais.")
+    else:
+        print("As duas strings são de tamanhos diferentes.")
+    if str1 == str2:
+        print("As duas strings possuem o mesmo conteúdo.")
+    else:
+        print("As duas strings possuem conteúdo diferente.")
+
+verificar_string(input("String 1: "),input("String 2: "))
