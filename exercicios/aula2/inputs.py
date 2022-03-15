c1c100b35a30b5b8fd25312a3bd956554f888e89
Filipe <lipe.msc@gmail.com>
2022-03-15 17:30:03 +0000

    Temporary progress from class. Gotta finish before next class

---
+++ exercicios/aula2/inputs.py
@@ -0,0 +1,25 @@
+def add_carro():
+    brand = input("Brand")
+    model = input("Model")
+    consumption = input("Consumption")
+    kms = input("KM's")
+
+def add_color():
+    name = input("Name")
+    r = input("R")
+    g = input("G")
+    b = input("B")
+
+def add_person():
+    forename = input("Forename")
+    surname = input("Surname")
+    address = input("Address")
+    cc = input("CC")
+    phonenumber = input("Phone Number")
+
+def add_engine():
+    fuel = input("Forename")
+    horsepower = input("Surname")
+    torque = input("Address")
+    displacement = input("CC")
+    number = input("Phone Number")
