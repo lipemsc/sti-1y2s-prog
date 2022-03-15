54a8e33d850bcd898fd141e39edef7ff29eb89d6
Filipe <lipe.msc@gmail.com>
2022-03-15 16:00:55 +0000

    Added file engine and modified file person. Classes done for now

---
+++ exercicios/aula2/engine.py
@@ -0,0 +1,70 @@
+class Engine:
+    def __init__(self, fuel, horsepower, torque, displacement,numbercilinders,
+        startingsystem, dryweight, manufacturer):
+        
+        self._fuel = fuel
+        self._horsepower = horsepower
+        self._torque = torque
+        self._displacement = displacement
+        self._numbercilinders = numbercilinders
+        self._startingsystem = startingsystem
+        self._dryweight = dryweight
+        self._manufacturer = manufacturer
+        
+    @property
+    def fuel(self):
+        return self._fuel
+
+    @fuel.setter
+    def fuel(self, fuel):
+        self._fuel = fuel
+    @property
+    def horsepower(self):
+        return self._horsepower
+
+    @horsepower.setter
+    def horsepower(self, horsepower):
+        self._horsepower = horsepower
+    @property
+    def torque(self):
+        return self._torque
+
+    @torque.setter
+    def torque(self, torque):
+        self._torque = torque
+    @property
+    def displacement(self):
+        return self._displacement
+
+    @displacement.setter
+    def displacement(self, displacement):
+        self._displacement = displacement
+    @property
+    def numbercilinders(self):
+        return self._numbercilinders
+
+    @numbercilinders.setter
+    def numbercilinders(self, numbercilinders):
+        self._numbercilinders = numbercilinders
+    @property
+    def startingsystem(self):
+        return self._startingsystem
+
+    @startingsystem.setter
+    def startingsystem(self, startingsystem):
+        self._startingsystem = startingsystem
+    @property
+    def dryweight(self):
+        return self._dryweight
+
+    @dryweight.setter
+    def dryweight(self, dryweight):
+        self._dryweight = dryweight
+    @property
+    def manufacturer(self):
+        return self._manufacturer
+
+    @manufacturer.setter
+    def manufacturer(self, manufacturer):
+        self._manufacturer = manufacturer
+    
--- exercicios/aula2/person.py
+++ exercicios/aula2/person.py
@@ -6,4 +6,44 @@ class Person:
         self._CC = CC
         self._phonenumber = phonenumber
     
+    @property
+    def forename(self):
+        return self._forename
+
+    @forename.setter
+    def forename(self, forename):
+        self._forename = forename
+    
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, surname):
+        self._surname = surname
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        self._address = address
+
+    @property
+    def CC(self):
+        return self._CC
+
+    @CC.setter
+    def CC(self, CC):
+        self._CC = CC
+
+    @property
+    def phonenumber(self):
+        return self._phonenumber
+
+    @phonenumber.setter
+    def phonenumber(self, phonenumber):
+        self._phonenumber = phonenumber
+
     
