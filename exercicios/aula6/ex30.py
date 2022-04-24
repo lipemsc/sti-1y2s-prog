ad930d3b98aeb8006e7f0c7c3075ef3cab3effb8
Filipe Coelho <lipe.msc@gmail.com>
2022-04-24 18:52:58 +0100

    Added more content to ex30 from aula 6

--- exercicios/aula6/ex30.py
+++ exercicios/aula6/ex30.py
@@ -34,15 +34,64 @@ class Adamastor:
         result = self.cursor.fetchall()
         return result
 
-    #def ex6(self):
+    def ex6(self):
+        self.cursor.execute("""
+                SELECT * 
+                FROM produtos
+                WHERE Existências < ExistênciaMínima
+        """)
+        result = self.cursor.fetchall()
+        return result
+
+    def ex10(self):
+        self.cursor.execute("""
+                SELECT * 
+                FROM encomendas
+                WHERE DataDaEncomenda BETWEEN "1996-07-10" AND "1996-07-17"
+        """)
+        result = self.cursor.fetchall()
+        return result
+
+    def ex12(self):
+        self.cursor.execute("""
+                SELECT encomendas.*
+                FROM
+                    encomendas INNER JOIN clientes
+                        ON encomendas.CódigoDoCliente = clientes.CódigoDoCliente
+                WHERE
+                    clientes.NomeDaEmpresa = 'Hanari Carnes'
+
+        """)
+        result = self.cursor.fetchall()
+        return result
+
+    def ex15(self):
+        self.cursor.execute("""
+                SELECT encomendas.*
+                FROM
+                    encomendas INNER JOIN clientes
+                        ON encomendas.CódigoDoCliente = clientes.CódigoDoCliente
+                WHERE
+                    clientes.NomeDaEmpresa = 'Hanari Carnes' AND
+                    encomendas.DataDaEncomenda = '1996-07-08'
+
+        """)
+        result = self.cursor.fetchall()
+        return result
+
+    #def ex21(self):
     #    self.cursor.execute("""
-    #            SELECT * 
-    #            FROM produtos
-    #            ORDER BY PreçoUnitário, NomeDoProduto
+    #            SELECT encomendas.*
+    #            FROM
+    #                encomendas INNER JOIN clientes
+    #                    ON encomendas.CódigoDoCliente = clientes.CódigoDoCliente
+    #            WHERE
+    #                clientes.NomeDaEmpresa = 'Hanari Carnes' AND
+    #                encomendas.DataDaEncomenda = '1996-07-08'
+
     #    """)
     #    result = self.cursor.fetchall()
     #    return result
-
 
 adam = Adamastor(config)
 
@@ -50,6 +99,17 @@ adam = Adamastor(config)
 # print(tabulate(adam.ex1(), headers="keys", tablefmt="psql"))
 
 # ex3
-print(tabulate(adam.ex3(), headers="keys", tablefmt="psql"))
+# print(tabulate(adam.ex3(), headers="keys", tablefmt="psql"))
 
 # ex6
+# print(tabulate(adam.ex6(), headers="keys", tablefmt="psql"))
+
+# ex10
+# print(tabulate(adam.ex10(), headers="keys", tablefmt="psql"))
+
+# ex12
+# print(tabulate(adam.ex12(), headers="keys", tablefmt="psql"))
+
+# ex15
+# print(tabulate(adam.ex15(), headers="keys", tablefmt="psql"))
+
