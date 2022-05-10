f87cae58471695935b343a338caa2b855721f10a
Filipe Coelho <lipe.msc@gmail.com>
2022-05-10 17:00:11 +0100

    Added example 7.
    BIG ONE

---
+++ aulas/aulaGUI/ex7.py
@@ -0,0 +1,70 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+
+
+class MyScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        grid = GridLayout(cols=2)
+        self.add_widget(grid)
+        self.orientation = "vertical"
+        
+        grid.add_widget(Label(text="Nome"))
+        self.txtnome = TextInput(multiline=False)
+        grid.add_widget(self.txtnome)
+
+        grid.add_widget(Label(text="Email"))
+        self.txtemail = TextInput(multiline=False)
+        grid.add_widget(self.txtemail)
+
+        grid.add_widget(Label(text="Tem carro"))
+        self.cbcarro = CheckBox()
+        grid.add_widget(self.cbcarro)
+
+        grid.add_widget(Label(text="Genero"))
+        grelhagenero = GridLayout(cols=2)
+        grid.add_widget(grelhagenero)
+
+        grelhagenero.add_widget(Label(text="Masculino"))
+        grelhagenero.add_widget(Label(text="Feminino"))
+
+        self.rbgenmasc = CheckBox(group="genero")
+        self.rbgenfem = CheckBox(group="genero")
+
+        grelhagenero.add_widget(self.rbgenmasc)
+        grelhagenero.add_widget(self.rbgenfem)
+
+        grid.add_widget(Label(text="Pais"))
+        self.spais = Spinner(text="Escolha os paises", values=["Portugal", "Espanha"])
+        grid.add_widget(self.spais)
+
+        grelhabotoes = GridLayout(cols=5)
+        self.add_widget(grelhabotoes)
+
+        self.bnovo = Button(text="Novo")
+        grelhabotoes.add_widget(self.bnovo)
+
+        self.bapagar = Button(text="Apagar")
+        grelhabotoes.add_widget(self.bapagar)
+
+        self.bgravar = Button(text="Gravar")
+        grelhabotoes.add_widget(self.bgravar)
+
+        self.banterior = Button(text="Anterior")
+        grelhabotoes.add_widget(self.banterior)
+
+        self.bproximo = Button(text="Próximo")
+        grelhabotoes.add_widget(self.bproximo)
+
+
+class MyApp(App):
+    def build(self):
+        return MyScreen()
+
+MyApp().run()
