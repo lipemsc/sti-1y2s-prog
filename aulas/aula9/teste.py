877aafcd586df3abe7d89f2f1416cb34d4d68743
Filipe Coelho <lipe.msc@gmail.com>
2022-05-17 15:42:54 +0100

    Updated aula 9

--- aulas/aula9/teste.py
+++ aulas/aula9/teste.py
@@ -3,11 +3,42 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class Formulario(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.lista_pessoas = [
+            Pessoa(nome="aaa", email="aaaa"),
+            Pessoa(nome="bbb", email="bbbb"),
+        ]
+        self.idx = 0
+
+    def clique(self, bt):
+        if bt == "novo":
+            print(1)
+        elif bt == "apaga":
+            print(2)
+        elif bt == "anterior":
+            self.idx = (self.idx - 1) % len(self.lista_pessoas)
+        elif bt == "proximo":
+            self.idx = (self.idx + 1) % len(self.lista_pessoas)
+        
+        self.preenche_formulario()
+
+    def preenche_formulario(self):
+        pessoa = self.lista_pessoas[self.idx]
+        self.ids.nome.text = pessoa.nome
+        self.ids.email.text = pessoa.email
+    
+
+class Pessoa:
+    def __init__(self,nome,email):
+        self.email = email
+        self.nome = nome
+
 
 class MyApp(App):
     def build(self):
         return Formulario()
 
+        
 
 MyApp().run()
