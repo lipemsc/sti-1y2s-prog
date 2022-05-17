989537086ec4325b1528176f4bab18acb33e9d79
Filipe Coelho <lipe.msc@gmail.com>
2022-05-17 16:00:36 +0100

    Updated aula 9

--- aulas/aula9/teste.py
+++ aulas/aula9/teste.py
@@ -6,16 +6,23 @@ class Formulario(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.lista_pessoas = [
-            Pessoa(nome="aaa", email="aaaa"),
-            Pessoa(nome="bbb", email="bbbb"),
+            Pessoa(nome="aaa", email="aaaa", tem_carro = True),
+            Pessoa(nome="bbb", email="bbbb", tem_carro = False),
         ]
         self.idx = 0
 
     def clique(self, bt):
         if bt == "novo":
-            print(1)
+            pessoa = Pessoa("?", "?", False)
+            self.lista_pessoas.append(pessoa)
+            self.idx = len(self.lista_pessoas) - 1
         elif bt == "apaga":
             print(2)
+        elif bt == "grava":
+            pessoa = self.lista_pessoas[self.idx]
+            pessoa.nome = self.ids.nome.text
+            pessoa.email = self.ids.email.text
+            pessoa.tem_carro = self.ids.tem_carro.active
         elif bt == "anterior":
             self.idx = (self.idx - 1) % len(self.lista_pessoas)
         elif bt == "proximo":
@@ -30,9 +37,10 @@ class Formulario(BoxLayout):
     
 
 class Pessoa:
-    def __init__(self,nome,email):
+    def __init__(self, nome, email, tem_carro):
         self.email = email
         self.nome = nome
+        self.tem_carro = tem_carro
 
 
 class MyApp(App):
