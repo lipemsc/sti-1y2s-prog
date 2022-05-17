c91c799b1ed7bb8e42307752b7da615be483b2a9
Filipe Coelho <lipe.msc@gmail.com>
2022-05-17 16:50:25 +0100

    Updated aula 9 once again aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- aulas/aula9/teste.py
+++ aulas/aula9/teste.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from kivy.app import App
 
 from kivy.uix.boxlayout import BoxLayout
@@ -6,8 +8,8 @@ class Formulario(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.lista_pessoas = [
-            Pessoa(nome="aaa", email="aaaa", tem_carro = True),
-            Pessoa(nome="bbb", email="bbbb", tem_carro = False),
+            Pessoa(nome="aaaaaa", email="aaaaaaa", tem_carro = True, genero = "M", pais="EN"),
+            Pessoa(nome="bbbbbb", email="bbbbbbb", tem_carro = False, genero = "M", pais="PT"),
         ]
         self.idx = 0
 
@@ -23,24 +25,41 @@ class Formulario(BoxLayout):
             pessoa.nome = self.ids.nome.text
             pessoa.email = self.ids.email.text
             pessoa.tem_carro = self.ids.tem_carro.active
+            pessoa.pais = self.ids.sp.text
+            pessoa.genero = "M" if self.ids.gen_m.active else "F"
         elif bt == "anterior":
             self.idx = (self.idx - 1) % len(self.lista_pessoas)
         elif bt == "proximo":
             self.idx = (self.idx + 1) % len(self.lista_pessoas)
-        
+        pprint(vars(self.lista_pessoas[self.idx]))
         self.preenche_formulario()
 
     def preenche_formulario(self):
-        pessoa = self.lista_pessoas[self.idx]
-        self.ids.nome.text = pessoa.nome
-        self.ids.email.text = pessoa.email
+        if len(self.lista_pessoas) >= 0:
+            pessoa = self.lista_pessoas[self.idx]
+            self.ids.nome.text = pessoa.nome
+            self.ids.email.text = pessoa.email
+            self.ids.tem_carro.active = pessoa.tem_carro
+            self.ids.sp.text = pessoa.pais
+            self.ids.gen_m.active = (pessoa.genero == "M")
+            self.ids.gen_f.active = not (pessoa.genero == "M")
+        else:
+            self.ids.nome.disabled = True
+            self.ids.email.disabled = True
+            self.ids.tem_carro.disabled = True
+            self.ids.sp.disabled = True
+            self.ids.gen_m.disabled = True
+            self.ids.gen_f.disabled = True
+
     
 
 class Pessoa:
-    def __init__(self, nome, email, tem_carro):
+    def __init__(self, nome, email, tem_carro, genero, pais):
         self.email = email
         self.nome = nome
         self.tem_carro = tem_carro
+        self.genero = genero
+        self.pais = pais
 
 
 class MyApp(App):
