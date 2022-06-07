08c39f3d1b36041688af128184f19ae7a051531c
Filipe Coelho <lipe.msc@gmail.com>
2022-06-07 16:48:06 +0100

    Added 35 - b)

---
+++ exercicios/aula10/b/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+class BApp(App):
+    def build(self):
+        return Layout()
+
+class Layout(BoxLayout):
+    def update_text(self):
+        self.ids.textbox2.text = self.ids.textbox.text[::-1]
+        
+    pass
+
+
+BApp().run()
