7ed5efe360679e1b61c86fca6da8c8171ce13446
Filipe Coelho <lipe.msc@gmail.com>
2022-06-08 15:26:52 +0100

    Fixed yesterday problem.
    So apparently, I can't have the name Layout. All I needed to do was
    changing it and it started working again. Kivy... you could have
    provided more hints, dont you think?

--- exercicios/aula10/e/main.py
+++ exercicios/aula10/e/main.py
@@ -1,14 +1,19 @@
 from kivy.app import App
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 class EApp(App):
     def build(self):
-        return Layout()
+        return MyLayout()
 
-class Layout(BoxLayout):
+class MyLayout(BoxLayout):
     def switch_text(self):
-        pass
+        tmp = self.ids.textbox.text
+        self.ids.textbox.text = self.ids.textbox2.text
+        self.ids.textbox2.text = tmp
 
 
 EApp().run()
