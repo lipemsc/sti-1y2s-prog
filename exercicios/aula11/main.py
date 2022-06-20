3feede397e3fdeb0548eefc2dc548b04b9732e4d
Filipe Coelho <lipe.msc@gmail.com>
2022-06-20 19:03:04 +0100

    I think it's almost done

--- exercicios/aula11/main.py
+++ exercicios/aula11/main.py
@@ -13,9 +13,13 @@ class GaloApp(App):
         return GaloLayout()
 
 class GaloLayout(BoxLayout):
+
+    MAX_GAMES = 3
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.game = 0
+        self.score = []
         self.lbl0 = Label(text="Jogador 1")
         self.lbl1 = Label(text="Jogador 2")
         self.ids.bl0.add_widget(self.lbl0)
@@ -28,9 +32,16 @@ class GaloLayout(BoxLayout):
         self.newgame()
         self.show_current_player()
 
+    def savescore(self):
+        try:
+            self.score.append(self.galo.winner)
+        except AttributeError:
+            print("no previous galo object, continuing")
+
     def newgame(self):
         self.galo = Galo()
         self.game += 1
+        print(self.score)
 
     def play(self, instance):
         for btn_no in range(len(self.posbtns)): 
@@ -58,24 +69,37 @@ class GaloLayout(BoxLayout):
         if self.galo.winner != 0:
             for btn in self.posbtns:
                 btn.disabled = True
+            
+            self.savescore()
+            # from here to line 91 creates a popup that shows once each game ends
             winpopup = Popup(
                 title=f"Jogador {self.galo.winner} ganhou!",
-                size=(200, 100),
+                size=(200, 200),
                 size_hint=(None,None),
                 auto_dismiss=False)
-            winpopup_btn = Button(text="Continuar")
+            winpopup_btn = Button(text="Continuar", size_hint_y = .3)
             winpopup_btn.bind(on_release=winpopup.dismiss)
             winpopup_bl = BoxLayout(orientation="vertical")
-            winpopup_bl.add_widget(Label(text="score"))
+            winpopup_lbl = Label(text=f"Jogador 1: {self.score.count(1)}\nJogador 2: {self.score.count(2)}")
+            winpopup_bl.add_widget(winpopup_lbl)
+            if self.MAX_GAMES < self.game:
+                if self.score.count(1) >= self.score.count(2):
+                    winner = 1
+                else:
+                    winner = 2
+                winpopup_lbl.text += f"\nParabéns jogador {winner}"
             winpopup_bl.add_widget(winpopup_btn)
-            winpopup.add_widget(winpopup_bl)
+            winpopup.content = winpopup_bl
             winpopup.bind(on_dismiss=self.reset)
             winpopup.open()
             #print(self.ids.lbl0)
     
     def reset(self, instance):
-        for btn in self.posbtns:
-                btn.disabled = False
-                btn.text = ""
+        if self.game < self.MAX_GAMES:
+            for btn in self.posbtns:
+                    btn.disabled = False
+                    btn.text = ""
+            self.newgame()
+            self.show_current_player()
 
 GaloApp().run()
