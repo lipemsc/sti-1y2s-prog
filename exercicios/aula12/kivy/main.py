d030187af2e64947ed4369f1d2763820e80056a5
Filipe Coelho <lipe.msc@gmail.com>
2022-06-27 20:36:50 +0100

    Multiple changes. Had to forget the idea of making a fancy GUI.
    Teacher, you want a fancy GUI, you do it yourself. The API is done.
    :stuck_out_tongue_closed_eyes:

--- exercicios/aula12/kivy/main.py
+++ exercicios/aula12/kivy/main.py
@@ -1,11 +1,7 @@
 from kivy.app import App
 from kivy.config import Config
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.button import Label
-from kivy.uix.popup import Popup
+
 
 from torneio import TorneiosLayout
 
--- exercicios/aula12/kivy/torneio.py
+++ exercicios/aula12/kivy/torneio.py
@@ -6,7 +6,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.button import Label
 from kivy.uix.popup import Popup
-from kivy.uix.scrollview import ScrollView
 
 import requests, json
 
@@ -14,18 +13,8 @@ import requests, json
 class TorneiosLayout(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.bl = BoxLayout(
-            orientation="vertical",
-            padding=(10, 10),
-            
-        )
-        self.ids.scroller.add_widget(self.bl)
         self.update()
     
     def update(self):
         torneios = json.loads(requests.get("http://localhost:8080/torneios/").text)
-        for torneio in torneios:
-            self.bl.add_widget(Button(
-                size_hint_y=40,
-                text=torneio["nome"]
-            ))
+        
