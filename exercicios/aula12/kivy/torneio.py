60fa7c1fb21e077586cc531b933ce24795b85257
Filipe Coelho <lipe.msc@gmail.com>
2022-06-28 12:34:02 +0100

    A few changes to the program. It now can fetch, update and delete.
    I'm awful at making GUI's.

--- exercicios/aula12/kivy/torneio.py
+++ exercicios/aula12/kivy/torneio.py
@@ -6,18 +6,60 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.button import Label
 from kivy.uix.popup import Popup
+from dateutil import parser
+from datetime import datetime
 
 import requests, json
 
 
 class TorneiosLayout(BoxLayout):
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     
-    def update(self):
-        req = requests.get(f"http://localhost:8080/torneios/{int(self.ids.tb_id.text)}")
+    id = -1
+
+    def get(self):
+        self.id = int(self.ids.tb_id.text)
+        req = requests.get(f"http://localhost:8080/torneios/{self.id}")
         if req.status_code == 200:
-            torneios = json.loads(req.text)
-            print(torneios)
-        
-        
+            torneio = json.loads(req.text)
+            data_inicio = parser.parse(torneio["data_inicio"])
+            data_fim = parser.parse(torneio["data_fim"])
+            self.ids.tb_nome.text = torneio["nome"]
+            self.ids.tb_dt_fim.text = data_fim.strftime("%Y-%m-%d")
+            self.ids.tb_dt_inicio.text = data_inicio.strftime("%Y-%m-%d")
+            self.ids.tb_local.text = torneio["local"]
+            self.ids.tb_nome.disabled = False
+            self.ids.tb_dt_fim.disabled = False
+            self.ids.tb_dt_inicio.disabled = False
+            self.ids.tb_local.disabled = False
+        else:
+            self.ids.tb_nome.text = ""
+            self.ids.tb_dt_fim.text = ""
+            self.ids.tb_dt_inicio.text = ""
+            self.ids.tb_local.text = ""
+            # self.ids.tb_local.disabled = True
+            # self.ids.tb_nome.disabled = True
+            # self.ids.tb_dt_fim.disabled = True
+            # self.ids.tb_dt_inicio.disabled = True
+
+
+    def new(self):
+        pass
+    def update(self):
+        upload = {}
+        upload["nome"] = self.ids.tb_nome.text
+        upload["data_fim"] = self.ids.tb_dt_fim.text
+        upload["data_inicio"] = self.ids.tb_dt_inicio.text
+        upload["local"] = self.ids.tb_local.text
+        print(upload)
+        response = requests.put(
+            f"http://localhost:8080/torneios/{self.id}",
+            data=json.dumps(upload),
+            headers={"Content-Type": "application/json"})
+        self.get()
+
+    def delete(self):
+        requests.delete(f"http://localhost:8080/torneios/{self.id}")
+        self.get()
