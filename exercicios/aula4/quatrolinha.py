81486bb68bbe1feeab62a7f5bd624c7e20b5ced1
Filipe Coelho <lipe.msc@gmail.com>
2022-07-01 16:07:00 +0100

    Aula 4 incomplete.
    Meanwhile the teacher has extended the deadline for assingment delivery,
    but I am still going to deliver today, and considered it finished.
    I have spend way too many brain cells coding this and I just want to
    consider it done so that I can move on.
    Gotta start planning my next programming adventure as well as my
    vacations :airplane:

--- exercicios/aula4/quatrolinha.py
+++ exercicios/aula4/quatrolinha.py
@@ -5,51 +5,118 @@ import random
 
 class QuatroLinha(Jogo):
     def inicializa_tabuleiro(self):
-        self.numero_de_jogadas_realizadas = 0  # conta as jogadas, serve para saber se ainda ha jogadas validas
-        self.tabuleiro = {(l, c): ' ' for l in range(6) for c in range(7)}  # o tabuleiro e um dicionario!
+        # conta as jogadas, serve para saber se ainda ha jogadas validas
+        self.numero_de_jogadas_realizadas = 0
+        self.tabuleiro = {(l, c): ' ' for l in range(6)
+                          for c in range(7)}  # o tabuleiro e um dicionario!
         # print(self.tabuleiro)
 
     def _le_linha_coluna_valida(self, s, axis='x'):
         """ metodo auxiliar para ler uma posicao que seja 0, 1 ou 2"""
         while True:
             x = input(s)
-            if x in ['0', '1', '2', '3', '4', '5', '6'] and axis=='x'\
-                or x in ['0', '1', '2', '3', '4', '5'] and axis=='y':
+            if x in ['0', '1', '2', '3', '4', '5', '6'] and axis == 'x'\
+                    or x in ['0', '1', '2', '3', '4', '5'] and axis == 'y':
                 return int(x)
 
     def joga_humano(self, jogador):
         print(f'jogador {jogador} insira a sua jogada')
         while True:
-            linha = self._le_linha_coluna_valida('Linha?', 'y')
             coluna = self._le_linha_coluna_valida('Coluna?', 'x')
 
-            # verifica se a posicao nao esta preenchida, i.e., e valida
-            if self.tabuleiro[(linha, coluna)] == ' ':
-                self.tabuleiro[(linha, coluna)] = ['X', 'O'][jogador]
-                self.numero_de_jogadas_realizadas += 1
-                return
+            for i in range(6):
+                if self.tabuleiro[(i, coluna)] != ' ':
+                    self.tabuleiro[(i-1, coluna)] = 'X' if jogador == 1 else 'O'
+                    break
             else:
-                print('Jogada invalida. Tente de novo')
+                self.tabuleiro[(i, coluna)] = 'X' if jogador == 1 else 'O'
+            break
+
+
+            # # verifica se a posicao nao esta preenchida, i.e., e valida
+            # if self.tabuleiro[(linha, coluna)] == ' ':
+            #     self.tabuleiro[(linha, coluna)] = ['X', 'O'][jogador]
+            #     self.numero_de_jogadas_realizadas += 1
+            #     return
+            # else:
+            #     print('Jogada invalida. Tente de novo')
 
     def terminou(self):
-        lista_posicaoes_ganhadoras = (
-            ((0, 0), (0, 1), (0, 2)),  # linha 0
-            ((1, 0), (1, 1), (1, 2)),  # linha 1
-            ((2, 0), (2, 1), (2, 2)),  # linha 2
-            ((0, 0), (1, 0), (2, 0)),  # coluna 0
-            ((0, 1), (1, 1), (2, 1)),  # coluna 1
-            ((0, 2), (1, 2), (2, 2)),  # coluna 2
-            ((0, 0), (1, 1), (2, 2)),  # diagonal
-            ((0, 2), (1, 1), (2, 0)),  # anti-diagonal
-        )
+        #debug(positions)
+        count = [0, 0]
 
-        for teste in lista_posicaoes_ganhadoras:
-            if self.tabuleiro[teste[0]] == self.tabuleiro[teste[1]] == self.tabuleiro[teste[2]] \
-                    and self.tabuleiro[teste[0]] != ' ':
-                return True  # encontrou posicao ganhadora
+        # horizontal verification
+        for line in range(5):
+            for cell in range(6):
+                if self.tabuleiro[(line, cell)] == ' ':
+                    count = [0, 0]
+                elif self.tabuleiro[(line, cell)] == 'X':
+                    count[0] += 1
+                    count[1] = 0
+                elif self.tabuleiro[(line, cell)] == 'O':
+                    count[1] += 1
+                    count[0] = 0
+                if count[0] >= 4:
+                    return True
+                if count[1] >= 4:
+                    return True
+            #print(count)
+        
+        count = [0, 0]
+
+        for x in range(6):
+            for y in range(5):
+                if self.tabuleiro[(y, x)] == ' ':
+                    count = [0, 0]
+                elif self.tabuleiro[(y, x)] == 'X':
+                    count[0] += 1
+                    count[1] = 0
+                elif self.tabuleiro[(y, x)] == 'O':
+                    count[1] += 1
+                    count[0] = 0
+                if count[0] >= 4:
+                    return True
+                if count[1] >= 4:
+                    return True
+            #print(count)
+
+
+        for y in range(6):
+            for x in range(5):
+                count = [0, 0]
+                for i in range(4):
+                    try:
+                        if self.tabuleiro[(y+i, x+i)] == 'X':
+                            count[0] += 1
+                        if self.tabuleiro[(y+1, x+1)] == 'O':
+                            count[1] += 1
+                    except (IndexError, KeyError):
+                        continue
+                    if count[0] >= 4:
+                        return True
+                    if count[1] >= 4:
+                        return True
+                
+                count = [0, 0]
+                for i in range(4):
+                    try:
+                        if x-i < 0:
+                            continue
+                        if self.tabuleiro[(y+1, x-1)] == 'X':
+                            count[0] += 1
+                        if self.tabuleiro[(y+1, x-1)] == 'O':
+                            count[1] += 1
+                    except (IndexError, KeyError):
+                        pass
+                    if count[0] >= 4:
+                        return True
+                    if count[1] >= 4:
+                        return True
+
         return False
 
     def mostra_tabuleiro(self):
+        # print(self.tabuleiro)
         print((4*7+1) * '-')
         for l in range(6):
             for c in range(7):
